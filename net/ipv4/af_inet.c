90f4334f7cfaa27e493c08c3504e2f78f552a83f
Tkkg1994 <luca@grifodev.ch>
2018-06-30 03:32:34 +0400

    af_inet: Let knox_ncm be disablable

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -440,6 +440,8 @@ out_rcu_unlock:
 	goto out;
 }
 
+#ifdef CONFIG_KNOX_NCM
+/* START_OF_KNOX_NPA */
 /** The function is used to check if the ncm feature is enabled or not; if enabled then collect the socket meta-data information; **/
 static void knox_collect_metadata(struct socket *sock) {
     if(check_ncm_flag()) {
@@ -498,7 +500,8 @@ static void knox_collect_metadata(struct socket *sock) {
         }
     }
 }
-
+/* END_OF_KNOX_NPA */
+#endif
 /*
  *	The peer socket should always be NULL (or else). When we call this
  *	function we are destroying the object and from then on nobody
@@ -527,7 +530,9 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+#ifdef CONFIG_KNOX_NCM
 		knox_collect_metadata(sock);
+#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
