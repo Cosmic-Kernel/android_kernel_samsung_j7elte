934b368ab6ecd143b1f04b3f8511d9e24e1b85de
themagicalmammal <devildipan@gmail.com>
2020-08-08 20:58:11 +0530

    Revert "UPSTREAM: include/linux/dcache.h: use unsigned chars in struct name_snapshot"
    This reverts commit e98e4fcf624c9cbee143f806e9d204f022feee75.

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -416,11 +416,10 @@ static inline bool d_mountpoint(struct dentry *dentry)
 extern int sysctl_vfs_cache_pressure;
 
 struct name_snapshot {
-	const unsigned char *name;
-	unsigned char inline_name[DNAME_INLINE_LEN];
+	const char *name;
+	char inline_name[DNAME_INLINE_LEN];
 };
 void take_dentry_name_snapshot(struct name_snapshot *, struct dentry *);
 void release_dentry_name_snapshot(struct name_snapshot *);
-};
 
 #endif	/* __LINUX_DCACHE_H */
